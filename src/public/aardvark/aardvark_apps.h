3c209a0cf155f2a7ac1bc6e39ff49fe8541a0cd7
Joe Ludwig <joe@valvesoftware.com>
2019-04-13 15:38:24 -0700

    Created aardvark_types.h
    Added a header file for global types and moved the error enum there.

--- src/public/aardvark/aardvark_apps.h
+++ src/public/aardvark/aardvark_apps.h
@@ -2,18 +2,12 @@
 
 #include <stdint.h>
 
+#include "aardvark_types.h"
+
 namespace aardvark
 {
 	class AppHandleClass_t;
 	typedef AppHandleClass_t *AppHandle_t;
-
-	enum EAardvarkError
-	{
-		AardvarkError_None = 0,
-
-		AardvarkError_InvalidParam = 1,
-		AardvarkError_InternalError = 2,
-	};
 
 	EAardvarkError avCreateApp( const char *appName, AppHandle_t *pHandle );
 	EAardvarkError avDestroyApp( AppHandle_t app );
---
+++ src/public/aardvark/aardvark_types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace aardvark
+{
+
+	enum EAardvarkError
+	{
+		AardvarkError_None = 0,
+
+		AardvarkError_InvalidParam = 1,
+		AardvarkError_InternalError = 2,
+	};
+
+}
+
