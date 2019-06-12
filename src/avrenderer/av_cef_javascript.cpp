78f81a4b3f60c79f8c1b2530f30b974ecddd5fdc
Joe Ludwig <joe@valvesoftware.com>
2019-06-11 21:01:36 -0700

    Don't try to clean up apps on exit
    Cap'n Proto is too far gone and they just fail. We'll clean these up on the server when the client disconnects once we switch to !EasyRPC

--- src/avrenderer/av_cef_javascript.cpp
+++ src/avrenderer/av_cef_javascript.cpp
@@ -452,8 +452,6 @@ void CAardvarkAppObject::cleanup()
 	}
 	m_sceneContexts.clear();
 
-	m_appClient.destroyRequest().send().wait( m_handler->getClient()->WaitScope() );
-
 	m_appClient = nullptr;
 }
 
