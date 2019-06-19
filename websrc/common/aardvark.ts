668d3d61ee47c782c9f29ea8955610740c92fb02
Joe Ludwig <joe@valvesoftware.com>
2019-06-19 08:34:59 -0700

    Added typescript declarations for javascript interface

--- websrc/common/aardvark.ts
+++ websrc/common/aardvark.ts
@@ -1,10 +1,3 @@
-declare global
-{
-	interface Window
-	{
-		aardvark: any;
-	}
-}
 
 export interface PokerProximity
 {
@@ -14,7 +7,138 @@ export interface PokerProximity
 	distance: number;
 }
 
-export function Av()
+interface AvSceneContext_Finish
 {
-	return window.aardvark as any;
+	():void;
+}
+
+export enum AvNodeType
+{
+	Container = 1,
+	Origin = 1,
+	Transform = 2,
+	Model = 3,
+	Panel = 4,
+	Poker = 5,
+}
+
+interface AvSceneContext_StartNode
+{
+	( nodeId: number, nodeName: string, nodeType:AvNodeType):void;
+}
+
+interface AvSceneContext_FinishNode
+{
+	():void;
+}
+
+interface AvSceneContext_SetOriginPath
+{
+	( path: string ): void;
+}
+
+interface AvSceneContext_SetTranslation
+{
+	( x: number, y: number, z: number ):void;
+}
+
+interface AvSceneContext_SetScale
+{
+	( x: number, y: number, z: number ):void;
+}
+
+interface AvSceneContext_SetUniformScale
+{
+	( scale: number ):void;
+}
+
+interface AvSceneContext_SetRotationEulerDegrees
+{
+	( yaw:number, pitch:number, roll:number):void;
+}
+
+interface AvSceneContext_SetModelUri
+{
+	(modelUri:string):void;
+}
+
+interface AvSceneContext_SetTextureSource
+{
+	(textureSource:string):void;
+}
+
+interface AvSceneContext_SetInteractive
+{
+	(interactive:boolean):void;
+}
+
+export interface AvSceneContext
+{
+	finish: AvSceneContext_Finish;
+	startNode: AvSceneContext_StartNode;
+	finishNode: AvSceneContext_FinishNode;
+	setOriginPath: AvSceneContext_SetOriginPath;
+	setTranslation: AvSceneContext_SetTranslation;
+	setScale: AvSceneContext_SetScale;
+	setUniformScale: AvSceneContext_SetUniformScale;
+	setRotationEulerDegrees: AvSceneContext_SetRotationEulerDegrees;
+	setModelUri: AvSceneContext_SetModelUri;
+	setTextureSource: AvSceneContext_SetTextureSource;
+	setInteractive: AvSceneContext_SetInteractive;
+}
+
+interface AvApp_GetName
+{
+	():string;
+}
+
+interface AvApp_StartSceneContext
+{
+	():AvSceneContext;
+}
+
+export interface AvPokerHandler
+{
+	( proximity: PokerProximity[] ): void;
+}
+
+interface AvApp_RegisterPokerHandler
+{
+	( nodeId:number, handlerFunction: AvPokerHandler ): void;
+}
+
+export interface AvApp
+{
+	getName: AvApp_GetName;
+	startSceneContext: AvApp_StartSceneContext;
+	registerPokerHandler: AvApp_RegisterPokerHandler;
+}
+
+interface Av_CreateApp
+{
+	(appName:string):AvApp;
+}
+
+interface Av_StartApp
+{
+	( uri:string, permissions: string[]):void;
+}
+
+export interface Aardvark
+{
+	createApp: Av_CreateApp;
+	startApp: Av_StartApp;
+}
+
+declare global
+{
+	interface Window
+	{
+		aardvark: any;
+	}
+}
+
+export function Av():Aardvark
+{
+	return window.aardvark as Aardvark;
 }
