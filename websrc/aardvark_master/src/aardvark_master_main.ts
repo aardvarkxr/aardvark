9fb11e850b6c2b6a1318bb929af799fa87874f40
Joe Ludwig <joe@valvesoftware.com>
2019-08-04 09:29:18 -0700

    Now commented out debugging stuff

--- websrc/aardvark_master/src/aardvark_master_main.ts
+++ websrc/aardvark_master/src/aardvark_master_main.ts
@@ -5,4 +5,5 @@ Av().startGadget( "http://aardvark.install/gadgets/default_hand", "/user/hand/ri
 Av().startGadget( "https://aardvark.install/gadgets/default_hand", "/user/hand/left", null );
 Av().startGadget( "https://aardvark.install/gadgets/test_gadget_launcher", "/user/hand/left", null );
 //Av().startGadget( "https://aardvark.install/gadgets/test_panel", "/user/hand/left", null );
+//Av().startGadget( "https://aardvark.install/gadgets/test_panel", "/user/hand/right", null );
 Av().startGadget( "https://aardvark.install/gadgets/charm_bracelet", "/user/hand/left", null );
