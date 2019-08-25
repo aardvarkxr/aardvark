d982ae2bc0f07fa015a9f307c1ee4296d4c66792
Joe Ludwig <joe@valvesoftware.com>
2019-08-25 10:03:17 -0700

    Removed unused import

--- websrc/common/aardvark-react/aardvark_standard_hook.tsx
+++ websrc/common/aardvark-react/aardvark_standard_hook.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import  * as ReactDOM from 'react-dom';
 import { AvTransform } from 'common/aardvark-react/aardvark_transform';
 import bind from 'bind-decorator';
 import { AvModel } from 'common/aardvark-react/aardvark_model';
