83265264c9061b564e8882b109cd39f9609a6f4e
Joe Ludwig <joe@valvesoftware.com>
2019-08-25 09:04:56 -0700

    Switched up what appears by default
    It's just hands and the new control panel now.  Control panel will soon gain the ability to spawn gadgets, including the charm bracelet and test panel, so the user will be able to switch back to the old stuff soon enough.

--- websrc/aardvark_master/src/aardvark_master_main.ts
+++ websrc/aardvark_master/src/aardvark_master_main.ts
@@ -3,7 +3,8 @@ import { Av } from 'common/aardvark'
 Av().startGadget( "http://aardvark.install/gadgets/aardvark_renderer", "", null );
 Av().startGadget( "http://aardvark.install/gadgets/default_hand", "/user/hand/right", null );
 Av().startGadget( "https://aardvark.install/gadgets/default_hand", "/user/hand/left", null );
-Av().startGadget( "https://aardvark.install/gadgets/test_gadget_launcher", "/user/hand/left", null );
+Av().startGadget( "https://aardvark.install/gadgets/control_panel", "/user/hand/left", null );
+//Av().startGadget( "https://aardvark.install/gadgets/test_gadget_launcher", "/user/hand/left", null );
 //Av().startGadget( "https://aardvark.install/gadgets/test_panel", "/user/hand/left", null );
 //Av().startGadget( "https://aardvark.install/gadgets/test_panel", "/user/hand/right", null );
-Av().startGadget( "https://aardvark.install/gadgets/charm_bracelet", "/user/hand/left", null );
+//Av().startGadget( "https://aardvark.install/gadgets/charm_bracelet", "/user/hand/left", null );
