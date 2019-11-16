e8d654a8aa68e43fe4c77cb025a0e7920e63eb3e
Joe Ludwig <joe@valvesoftware.com>
2019-11-16 12:59:30 -0800

    Added gadget validity checking to install
    If <url>/gadget_manifest.json doesn't exist, the gadget won't be added.
    
    Fixes #31

--- packages/aardvark-cli/src/avcmd.ts
+++ packages/aardvark-cli/src/avcmd.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-
-import { getStandardAardvarkPath, getStandardPersistencePath, readPersistentState, writePersistentState } from '@aardvarkxr/aardvark-shared';
+import { URL } from 'url';
+import axios, { AxiosResponse } from 'axios';
+import { getStandardAardvarkPath, getStandardPersistencePath, readPersistentState, 
+	writePersistentState, AvGadgetManifest } from '@aardvarkxr/aardvark-shared';
 import * as fs from 'fs';
 import fileUrl from 'file-url';
 import isUrl from 'is-url';
@@ -9,6 +11,8 @@ let program = require( 'commander' );
 
 
 let showHelp = true;
+
+let asyncCommand: null | { (): void} = null;
 
 program
 	.version( '0.1.0' )
@@ -19,36 +23,51 @@ program
 	.command( "install [url...]" )
 	.action( ( urls: string[] ) =>
 	{
-		let aardvarkPath = getStandardAardvarkPath();
-		let persistencePath = getStandardPersistencePath();
-
-		let state = readPersistentState( persistencePath );
-
-		let dirty = false;
-		for( let gadgetUrl of urls )
+		asyncCommand = async () =>
 		{
-			if( !isUrl( gadgetUrl ) )
+			let aardvarkPath = getStandardAardvarkPath();
+			let persistencePath = getStandardPersistencePath();
+	
+			let state = readPersistentState( persistencePath );
+	
+			let dirty = false;
+			for( let gadgetUrl of urls )
 			{
-				// turn paths into URLs
-				gadgetUrl = fileUrl( gadgetUrl );
+				if( !isUrl( gadgetUrl ) )
+				{
+					// turn paths into URLs
+					gadgetUrl = fileUrl( gadgetUrl );
+				}
+	
+				try
+				{
+					let gadgetManifest: AvGadgetManifest = await getJSONFromUri( gadgetUrl + "/gadget_manifest.json" ) as AvGadgetManifest
+					if( !gadgetManifest || !gadgetManifest.name )
+					{
+						throw "Invalid manifest";
+					}
+					if( state.installedGadgets.includes( gadgetUrl ) )
+					{
+						console.log( `${ gadgetManifest.name } is already installed: ${ gadgetUrl }` );
+					}
+					else
+					{
+						console.log( `Installing ${ gadgetManifest.name }: ${ gadgetUrl }` );
+						state.installedGadgets.push( gadgetUrl );
+						dirty = true;
+					}
+				}
+				catch( e )
+				{
+					console.log( `Invalid gadget url ${ gadgetUrl }` );
+				}
 			}
-
-			if( state.installedGadgets.includes( gadgetUrl ) )
+	
+			if( dirty )
 			{
-				console.log( `Gadget is already installed: ${ gadgetUrl }` );
+				console.log( `writing state to ${ persistencePath }` );
+				writePersistentState( state, persistencePath );
 			}
-			else
-			{
-				console.log( `Installing gadget: ${ gadgetUrl }` );
-				state.installedGadgets.push( gadgetUrl );
-				dirty = true;
-			}
-		}
-
-		if( dirty )
-		{
-			console.log( `writing state to ${ persistencePath }` );
-			writePersistentState( state, persistencePath );
 		}
 
 		showHelp = false;
@@ -143,4 +162,49 @@ program.parse( process.argv );
 if( showHelp )
 {
 	program.outputHelp();
+}
+if( asyncCommand )
+{
+	( asyncCommand as () => void) ();
+}
+
+export function getJSONFromUri( uri: string ): Promise< any >
+{
+	return new Promise<any>( ( resolve, reject ) =>
+	{
+		try
+		{
+			let url = new URL( uri );
+			if( url.protocol == "file:" )
+			{
+				fs.readFile( url, "utf8", (err: NodeJS.ErrnoException | null, data: string ):void =>
+				{
+					if( err )
+					{
+						reject( err );
+					}
+					else
+					{
+						resolve( JSON.parse( data ) );
+					}
+				});
+			}
+			else
+			{
+				let promRequest = axios.get( url.toString() )
+				.then( (value: AxiosResponse ) =>
+				{
+					resolve( value.data );
+				} )
+				.catch( (reason: any ) =>
+				{
+					reject( reason );
+				});
+			}
+		}
+		catch( e )
+		{
+			reject( e );
+		}
+	} );
 }
