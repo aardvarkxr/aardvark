3df5286d75c978801c1b3c282a4a319a8e2f4558
Michael Boone <fuag155555@gmail.com>
2020-04-14 16:21:55 -0800

    resolved no object file error

--- src/public/aardvark/aardvark_renderer_config.h
+++ src/public/aardvark/aardvark_renderer_config.h
@@ -6,7 +6,7 @@ class CAardvarkRendererConfig
 {
 public:
 	bool m_bMixedRealityEnabled = false;
-	float_t m_fMixedRealityFOV = 50.3;
+	float_t m_fMixedRealityFOV = 50.3f;
 };
 
 void to_json( nlohmann::json & j, const CAardvarkRendererConfig& gm );
