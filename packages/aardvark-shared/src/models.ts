f0c952dc17e3b072cbf33eba7276a57b1488a584
Joe Ludwig <joe@valvesoftware.com>
2020-06-14 13:07:18 -0700

    Added an animated attract state model

--- packages/aardvark-shared/src/models.ts
+++ packages/aardvark-shared/src/models.ts
@@ -9,6 +9,7 @@ export const g_builtinModelArrow = g_builtinModelPrefix + "arrow.glb";
 export const g_builtinModelBoundingBox = g_builtinModelPrefix + "bounding_box.glb";
 export const g_builtinModelCylinder = g_builtinModelPrefix + "cylinder.glb";
 export const g_builtinModelBox = g_builtinModelPrefix + "box.glb";
+export const g_builtinModelDropAttract = g_builtinModelPrefix + "drop_attract.glb";
 export const g_builtinModelPlus = g_builtinModelPrefix + "plus.glb";
 export const g_builtinModelMinus = g_builtinModelPrefix + "minus.glb";
 export const g_builtinModelBracelet = g_builtinModelPrefix + "bracelet.glb";
