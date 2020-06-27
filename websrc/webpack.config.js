7a29740d64b4c1ca3a446b3cdabe6f1f38248894
Joe Ludwig <joe@valvesoftware.com>
2020-06-27 15:13:30 -0700

    Removed gadget installer gadget
    The new landing pages will serve this function.

--- websrc/webpack.config.js
+++ websrc/webpack.config.js
@@ -121,7 +121,6 @@ module.exports =
 	createConfig( 'aardvark_renderer', 'Renderer', 'ts' ),
 	createConfig( 'default_hands', 'Default Hands', 'tsx' ),
 	createConfig( 'test_panel', 'Test Panel', 'tsx' ),
-	createConfig( 'gadget_installer', 'Gadget Installer', 'tsx' ),
 	createConfig( 'gadget_menu', 'Gadget Menu', 'tsx' ),
 	createConfig( 'hand_mirror', 'Hand Mirror', 'tsx' ),
 	createConfig( 'control_test', 'Control Tester', 'tsx' ),
