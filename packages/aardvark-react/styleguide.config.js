0860f19f2d6889b35d39221bd3c8b4971de4b5ce
Joe Ludwig <joe@valvesoftware.com>
2020-06-27 15:46:15 -0700

    Fixed doc build

--- packages/aardvark-react/styleguide.config.js
+++ packages/aardvark-react/styleguide.config.js
@@ -28,7 +28,15 @@ module.exports =
 					use: 'ts-loader',
 					exclude: /node_modules/
 				},
-			]
+				{
+					test: /\.css$/,
+					use: 
+					[
+						'style-loader',
+						'css-loader'
+					]
+				},
+		]
 		},
 	}
 	
