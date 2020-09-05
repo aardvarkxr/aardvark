3b5437bb3379d636ea51d539fe0c2eb9a37ad34c
Joe Ludwig <joe@valvesoftware.com>
2020-09-05 12:09:16 -0700

    Fixed CSS include breaking tests

--- websrc/jest.config.js
+++ websrc/jest.config.js
@@ -6,11 +6,11 @@ module.exports = {
   moduleNameMapper: {
     '^@aardvarkxr/aardvark-shared$': path.resolve(__dirname, '../packages/aardvark-shared/src/index.ts'),
     '^@aardvarkxr/aardvark-react$': path.resolve(__dirname, '../packages/aardvark-react/src/index.ts'),
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
   modulePaths: 
   [
     __dirname, 
     path.resolve( __dirname, "./node_modules/" ),
   ]
-
 };
